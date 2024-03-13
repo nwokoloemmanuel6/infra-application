4f5be4da5ab22810150c3888806c9e310199c1ef
Nwokolo Emmanuel <nwokoloebuka84@gmail.com>
2024-03-13 13:50:18 +0100

    new change

--- modules/networking/main.tf
+++ modules/networking/main.tf
@@ -162,8 +162,8 @@ resource "aws_security_group" "three_tier_lb_sg" {
   vpc_id      = aws_vpc.three_tier_vpc.id
 
   ingress {
-    from_port   = 3000
-    to_port     = 3000
+    from_port   = 80
+    to_port     = 80
     protocol    = "tcp"
     cidr_blocks = ["0.0.0.0/0"]
   }
